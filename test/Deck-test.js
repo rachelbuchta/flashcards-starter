93caea4ce493c9b7150a709d150f8488cfe87bd0
Rachel Buchta <rachelbuchta@yahoo.com>
2020-12-01 18:18:43 -0700

    Add deck test

---
+++ test/Deck-test.js
@@ -0,0 +1,44 @@
+const chai = require('chai')
+const expect = chai.expect
+
+const Deck = require('../src/Deck')
+const Turn = require('../src/Turn')
+const Card = require('../src/Card')
+
+describe('Deck', () => {
+  // let card1;
+  // let card2;
+  // let card3;
+  // let deck;
+  //
+  // beforeEach(function() {
+  //   card1 = new Card(2, 'Question1', ['blue', 'red', 'yellow'], 'yellow')
+  //   card2 = new Card(4, 'Question2', ['correct','wrong','maybe'], 'correct')
+  //   card3 = new Card(6, 'Question3', ['up', 'down', 'around'], 'up')
+  //   deck = new Deck()
+  // })
+
+  it('should be a function', () => {
+    const deck = new Deck()
+
+    expect(Deck).to.be.a('function')
+  })
+
+  it('should instantiate an instance of Deck', () => {
+    card1 = new Card(2, 'Question1', ['blue', 'red', 'yellow'], 'yellow')
+    card2 = new Card(4, 'Question2', ['correct','wrong','maybe'], 'correct')
+    card3 = new Card(6, 'Question3', ['up', 'down', 'around'], 'up')
+    deck = new Deck([card1, card2, card3])
+
+    expect(deck).to.be.an.instanceof(Deck)
+  })
+
+  it('should know how many cards are in the deck', () => {
+    card1 = new Card(2, 'Question1', ['blue', 'red', 'yellow'], 'yellow')
+    card2 = new Card(4, 'Question2', ['correct','wrong','maybe'], 'correct')
+    card3 = new Card(6, 'Question3', ['up', 'down', 'around'], 'up')
+    deck = new Deck([card1, card2, card3])
+
+    expect(deck.countCards()).to.equal(3)
+  })
+})
